1086756b3f88fbfb594bc0ae9a46750fe385e469
OctavioNovelo <105056581+OctavioNovelo@users.noreply.github.com>
2023-04-17 14:55:26 -0600

    OMI Caracteres y Cadenas (Leccion)

---
+++ C++ Codes/Letters?.cpp
@@ -0,0 +1,26 @@
+/* Link: https://omegaup.com/course/Seleccion_YUC2023/assignment/Caracteres#problems */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char c;
+
+    cin >> c;
+
+    int i = (int)c;
+
+    if(c >= 65 && c <= 90){
+
+        cout << "Si";
+    }
+    else if(c >= 97 && c <= 122){
+
+        cout << "Si";
+    }
+    else{
+        cout << "No";
+    }
+}
