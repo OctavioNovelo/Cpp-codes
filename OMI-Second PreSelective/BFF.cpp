e7e703ddb853251e202fe1f96a3ead2adfb28ccc
OctavioNovelo <105056581+OctavioNovelo@users.noreply.github.com>
2023-05-14 20:09:04 -0600

    Second PreSelective Exercies for the OMI

--- OMI-Second PreSelective/BFF.cpp
+++ OMI-Second PreSelective/BFF.cpp
@@ -1,39 +1,41 @@
 /* Link: https://omegaup.com/arena/Yuc_2023_1/#problems/El-amigo-favorito */
-
 #include <iostream>
 
 using namespace std;
 
-int main(){
+int main() {
+    long long  D;
+    long long  N;
+    long long  K;
 
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    long long answer = 0;
 
+    cin >> N >> D >> K;
 
-    long long n, d, k;
-
-    cin >> n >> d >> k;
-
-    long long j = 0;
-    long long maxcandy = 1;
-    d -= n;
-    while(d > 0){
-        for(j = k; j < n -1; k++) {
-            d--;
-        }
-        for(j = k; j >= 0; j--){
-            d--;
-        }
-        if(d > 0){
-            j = k;
-        }
-        maxcandy++;
+    if (D < K) {
+        cout << 0 << endl;
+        return 0;
     }
+    else {
+        answer += 1;
+        D -= N;
 
-        if(d == 0){
-            cout << maxcandy << endl;
-            return 0;
+        long long left = K;
+        long long right = K;
+
+        while (D > 0) {
+            answer += 1;
+            D -= (right - left + 1);
+            left -= 1;
+            if (left < 0) {
+                left = 0;
+            }
+            right += 1;
+            if (right > N - 1) {
+                right = N - 1;
+            }
         }
-
+        cout << answer << endl;
+    }
     return 0;
 }
