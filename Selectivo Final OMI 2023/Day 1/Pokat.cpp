c51548d6a8d4a738476c12fc8850e2515246b542
OctavioNovelo <octavionovelo22@gmail.com>
2023-08-19 07:58:29 -0600

    '#'

--- Selectivo Final OMI 2023/Day 1/Pokat.cpp
+++ Selectivo Final OMI 2023/Day 1/Pokat.cpp
@@ -2,40 +2,65 @@
 
 using namespace std;
 
-int main(){
+    int n;
+    vector<int> grafo[5001]; /// lista de adyacencia
 
-    long long cats;
+    struct pos{ /// Estructura para hacer la BFS
+        int i; /// nodo del paso actual
+        int d; /// distancia en la que se encontro el nodo
+        pos(int i, int d) : i(i), d(d) {}
+    };
 
-    cin >> cats;
+    /// Hacemos la BFS para contar la minima recompensa de todos para la opcion actual
+    /// La funcion regresa la suma de recompensas minima para el ganador actual
+    int BFS(int gatoGanador){
+    vector<char> visitado(n + 2);
 
-    long long players[cats][cats];
+    queue<pos> cola;
+    cola.push( pos(gatoGanador, 1) ); /// iniciamos con el ganador de todos, con distancia 1
+    visitado[gatoGanador] = true;
 
-    for(int i = 0; i < cats; i++){
-        cin >> players[i][0];
-        for(int e = 0; e < players[i][0]; e++){
-            cin >> players[i][e];
+    int suma, cnt_nodos = suma = 0;
+    while(!cola.empty()){
+        pos act = cola.front();
+        cola.pop();
+        suma += act.d; /// sumamos las distancias de cada nodo visitado
+        cnt_nodos++; /// y contamos la cantidad de nodos que visitamos
+            for(int it : grafo[act.i]){
+                if(visitado[it]) continue;
+                visitado[it] = true;
+                pos nnp(it, act.d + 1);
+                cola.push(nnp);
+            }
+    }
+
+    /// Si i es un ganador total valido, entonces podremos llegar a todos los nodos con la BFS
+    /// Esto significa que suma es una posible respuesta
+    if(cnt_nodos == n) return suma;
+        return INT_MAX;
+    }
+
+    int main() {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+    cin >> n;
+
+    /// Guardamos el grafo
+    for(int i = 1; i <= n; ++i){
+        int k; cin >> k;
+        grafo[i].reserve(k);
+        while(k--){
+            int x; cin >> x;
+            grafo[x].push_back(i); /// x le puede ganar a i
         }
     }
 
-    long long bucket[cats];
-
-    for(int i = 0; i < cats; i++){
-        if(players[i][0] == 1){
-            if(players[i][1] == 1){
-                bucket[i + 1];
-            }
-        }
-        else if(players[i][0] > 1){
-            for(int e = 1; i < players[i][0]; i++){
-                if(players[i][e] == 2){
-                    bucket[e + 1];
-                }
-                else if(players[i][e] > sizeof(bucket)){
-                    bucket[cats - 1] + 1;
-                    bucket[(cats - 1) + 1];
-                }
-            }
-        }
-    }
-
+    int ans = 1e9;
+    
+    /// Probamos con cada gato como posible ganador
+    
+    for(int i = 1; i <= n; ++i) ans = min(ans, BFS(i));
+        cout << ans << '\n';
 }
