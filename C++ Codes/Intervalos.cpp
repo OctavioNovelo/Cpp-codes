d6de7a85c5c20203175e98ebfb7fa78002c9ac07
OctavioNovelo <octavionovelo22@gmail.com>
2024-02-08 21:08:12 -0600

    ...

---
+++ C++ Codes/Intervalos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+long long a, b, c;
+
+
+int main(){
+
+    cin >> a >> b >> c;
+
+    if(a, b, c){
+        cout << "IZQUIERDA";
+    }
+    else if(a, c, b){
+        cout << "DERECHA";
+    }
+    else{
+        cout << "INTERVALO";
+    }
+    return 0;
+    
+}
