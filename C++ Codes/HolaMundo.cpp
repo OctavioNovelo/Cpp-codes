17741e2ac5f0445b980c472ae4b94d216c8f5692
OctavioNovelo <octavionovelo22@gmail.com>
2024-03-01 12:56:37 -0600

    ...

--- C++ Codes/HolaMundo.cpp
+++ C++ Codes/HolaMundo.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 
-int Numero1 = 2;
-int Numero2 = 3;
-int Numero3 = 2;
-
-int resultado;
+using namespace std;
 
 int main(){
-
-resultado = Numero1 * Numero2 * Numero3;
-
-std::cout<<resultado;
-    
-    return 0;
+    cout << "Hola Mundo";
 }
